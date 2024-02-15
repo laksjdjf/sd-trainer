115e741a0e916794dec4ac91498738d238b5b411
laksjdjf <aikasama@hotmail.co.jp>
2024-02-15 12:06:14 +0900

    Update manager.py

--- networks/manager.py
+++ networks/manager.py
@@ -9,7 +9,6 @@ import logging
 logger = logging.getLogger("ネットワークちゃん")
 
 UNET_TARGET_REPLACE_MODULE_TRANSFORMER = ["Transformer2DModel"]
-UNET_TARGET_REPLACE_MODULE_ATTENTION = ["Transformer2DModel"]
 UNET_TARGET_REPLACE_MODULE_CONV = ["ResnetBlock2D", "Downsample2D", "Upsample2D"]
 TEXT_ENCODER_TARGET_REPLACE_MODULE = ["CLIPAttention", "CLIPMLP"]
 LORA_PREFIX_UNET = 'lora_unet'
